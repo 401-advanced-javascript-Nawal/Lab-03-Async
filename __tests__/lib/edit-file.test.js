afa7b5816f673ef90cc896e9ec1607d3a54236b2
Nsuliman <nsuliman88@gmail.com>
2020-01-24 05:38:04 +0200

    read file testing

---
+++ __tests__/lib/edit-file.test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const reader = require('../../lib/reader.js');
+
+describe(' FS file reader Module ' , () =>
+{
+
+    describe (' Call back reader' , () =>
+    {
+
+    })
+
+    describe (' Promise reader' , () =>
+    {
+        
+    })
+
+})
